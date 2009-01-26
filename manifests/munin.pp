02f78bcb4c3d9daab656cb1c254ca61f1b0a0996
mh <mh@immerda.ch>
2009-01-26 23:08:16 +0000

    fixing multiplugin

--- manifests/munin.pp
+++ manifests/munin.pp
@@ -2,16 +2,23 @@ class ejabberd::munin {
   case $ejabberd_domains {
     '': { $ejabberd_domains = $fqdn }
   }
-  munin::plugin::deploy{'ejabberd_users': 
+  munin::plugin::deploy{'ejabberd_': 
     source => "ejabberd/munin/ejabberd_", 
+    ensure => absent,
+  }
+  munin::plugin{'ejabberd_users': 
+    require => Munin::Plugin::Deploy['ejabberd_users'],
+    ensure => 'ejabberd_',
     config => "env.vhosts ${$ejabberd_domains}\nuser root" 
   }
   munin::plugin{'ejabberd_connections': 
     require => Munin::Plugin::Deploy['ejabberd_users'],
+    ensure => 'ejabberd_',
     config => "env.vhosts ${$ejabberd_domains}\nuser root" 
   }
   munin::plugin{'ejabberd_registrations': 
     require => Munin::Plugin::Deploy['ejabberd_users'],
+    ensure => 'ejabberd_',
     config => "env.vhosts ${$ejabberd_domains}\nuser root" 
   }
 }
