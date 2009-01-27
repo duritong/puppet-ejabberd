49ea4ff8b8ad4c9dab950ad6276ee2da0183af56
mh <mh@immerda.ch>
2009-01-27 09:27:31 +0000

    increasing plugin timeout, as user fetching might take some time

--- manifests/munin.pp
+++ manifests/munin.pp
@@ -9,7 +9,7 @@ class ejabberd::munin {
   munin::plugin{'ejabberd_users': 
     require => Munin::Plugin::Deploy['ejabberd_'],
     ensure => 'ejabberd_',
-    config => "env.vhosts ${ejabberd_domains}\nuser root" 
+    config => "env.vhosts ${ejabberd_domains}\ntimeout 20\nuser root" 
   }
   munin::plugin{'ejabberd_connections': 
     require => Munin::Plugin::Deploy['ejabberd_'],
