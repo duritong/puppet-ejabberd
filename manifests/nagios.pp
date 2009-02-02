42661f4f65df88965156c449bf042f0eee505775
mh <mh@immerda.ch>
2009-02-02 23:24:33 +0000

    correct naming

--- manifests/nagios.pp
+++ manifests/nagios.pp
@@ -13,7 +13,7 @@ class ejabberd::nagios {
     case $jabber_nagios_pwd {
         '': { info("no \$jabber_nagios_pwd supplied for ${fqdn}! Can't test jabber login") }
         default: { 
-            nagios::service{ "jabber_${fqdn}": check_command => "check_jabber_login!${jabber_nagios_user}@${jabber_nagios_domain}!${jabber_nagios_pwd}" }
+            nagios::service{ "jabber_login_${fqdn}": check_command => "check_jabber_login!${jabber_nagios_user}@${jabber_nagios_domain}!${jabber_nagios_pwd}" }
         }
     }
 }
