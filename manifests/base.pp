7e5f03cbdb939d6acb0e9cfb7dbdebcae604510d
mh <mh@immerda.ch>
2009-09-29 23:27:56 +0200

    add shorewall rule, refactor

---
+++ manifests/base.pp
@@ -0,0 +1,21 @@
+class ejabberd::base {
+    package{'ejabberd':
+        ensure => installed,
+    }
+
+    file{'/etc/ejabberd/ejabberd.cfg':
+      source => [ "puppet://$server/files/ejabberd/${fqdn}/ejabberd.cfg",
+                  "puppet://$server/files/ejabberd/ejabberd.cfg",
+                  "puppet://$server/ejabberd/ejabberd.cfg" ],
+      require => Package['ejabberd'],
+      notify => Service['ejabberd'],
+      owner => ejabberd, group => ejabberd, mode => 0640;
+    }
+
+    service{ejabberd:
+        ensure => running,
+        enable => true,
+        hasstatus => true, #fixme!
+        require => Package[ejabberd],
+    }
+}
--- manifests/init.pp
+++ manifests/init.pp
@@ -13,26 +13,8 @@ class ejabberd {
     if $use_munin {
         include ejabberd::munin
     }
-}
 
-class ejabberd::base {
-    package{'ejabberd':
-        ensure => installed,
-    }
-
-    file{'/etc/ejabberd/ejabberd.cfg':
-      source => [ "puppet://$server/files/ejabberd/${fqdn}/ejabberd.cfg",
-                  "puppet://$server/files/ejabberd/ejabberd.cfg",
-                  "puppet://$server/ejabberd/ejabberd.cfg" ],
-      require => Package['ejabberd'],
-      notify => Service['ejabberd'],
-      owner => ejabberd, group => ejabberd, mode => 0640;
-    }
-
-    service{ejabberd:
-        ensure => running,
-        enable => true,
-        hasstatus => true, #fixme!
-        require => Package[ejabberd],
+    if $use_shorewall {
+      include shorewall::rules::jabberserver
     }
 }
