36f40d47f3c583ec302a192f9940a32bfe66ae3f
mh <mh@immerda.ch>
2010-06-28 22:36:41 +0200

    deploy nagios plugin and define command

--- manifests/nagios.pp
+++ manifests/nagios.pp
@@ -10,6 +10,16 @@ class ejabberd::nagios {
         '': { $jabber_nagios_user = 'nagios' }
     } 
 
+    @@nagios::plugin{'check_jabber_login':
+      source => 'ejabberd/nagios/check_jabber_login';
+    }
+
+    @@nagios_command{
+      'check_jabber_login':
+            command_line => '$USER1$/check_jabber_login $ARG1$ $ARG2$',
+            require => Nagios::Plugin['check_jabber_login'];
+    }
+
     case $jabber_nagios_pwd {
         '': { info("no \$jabber_nagios_pwd supplied for ${fqdn}! Can't test jabber login") }
         default: { 
