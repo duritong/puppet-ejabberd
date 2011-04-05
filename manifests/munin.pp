b16aca869c080a7381c884e56ebbe66f928945e8
mh <mh@immerda.ch>
2011-04-05 22:10:01 +0200

    fix munin plugins for new versions

--- manifests/munin.pp
+++ manifests/munin.pp
@@ -6,19 +6,9 @@ class ejabberd::munin {
     source => "ejabberd/munin/ejabberd_", 
     ensure => absent,
   }
-  munin::plugin{'ejabberd_users': 
+  munin::plugin{['ejabberd_users','ejabberd_connections','ejabberd_registrations']:
     require => Munin::Plugin::Deploy['ejabberd_'],
     ensure => 'ejabberd_',
-    config => "env.vhosts ${ejabberd_domains}\ntimeout 20\nuser root" 
-  }
-  munin::plugin{'ejabberd_connections': 
-    require => Munin::Plugin::Deploy['ejabberd_'],
-    ensure => 'ejabberd_',
-    config => "env.vhosts ${ejabberd_domains}\nuser root" 
-  }
-  munin::plugin{'ejabberd_registrations': 
-    require => Munin::Plugin::Deploy['ejabberd_'],
-    ensure => 'ejabberd_',
-    config => "env.vhosts ${ejabberd_domains}\nuser root" 
+    config => "env.vhosts ${ejabberd_domains}\ntimeout 20\nuser root\ngroup root"
   }
 }
