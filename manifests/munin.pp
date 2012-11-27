9d0260a10ab737a4b1e57403fadcda95e15ce80b
mh <mh@immerda.ch>
2012-11-27 14:08:57 +0100

    improve performance of the ejabberd plugin

--- manifests/munin.pp
+++ manifests/munin.pp
@@ -1,12 +1,21 @@
+# install munin plugin and helpers
 class ejabberd::munin {
   munin::plugin::deploy{'ejabberd_':
-    source => "ejabberd/munin/ejabberd_",
     ensure => absent,
+    source => 'ejabberd/munin/ejabberd_';
   }
   $domains = join($ejabberd::domains, ' ')
   munin::plugin{['ejabberd_users','ejabberd_connections','ejabberd_registrations']:
-    require => Munin::Plugin::Deploy['ejabberd_'],
-    ensure => 'ejabberd_',
-    config => "env.vhosts ${domains}\ntimeout 30\nuser root\ngroup root"
+    ensure  => 'ejabberd_',
+    require => [ Munin::Plugin::Deploy['ejabberd_'] ],
+    config  => "env.vhosts ${domains}\ntimeout 30\nuser root\ngroup root";
+  }
+  file{
+    '/etc/cron.daily/ejabberd_registrations':
+      source  => 'puppet:///modules/ejabberd/munin/ejabberd_registrations.cron',
+      require => Munin::Plugin['ejabberd_registrations'],
+      owner   => root,
+      group   => 0,
+      mode    => '0700';
   }
 }
