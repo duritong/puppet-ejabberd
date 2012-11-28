b41dd54597cd925a3e26879ccf21fba24c435033
mh <mh@immerda.ch>
2012-11-28 23:53:19 +0100

    cronify all munin plugins

--- manifests/munin.pp
+++ manifests/munin.pp
@@ -8,14 +8,18 @@ class ejabberd::munin {
   munin::plugin{['ejabberd_users','ejabberd_connections','ejabberd_registrations']:
     ensure  => 'ejabberd_',
     require => [ Munin::Plugin::Deploy['ejabberd_'] ],
-    config  => "env.vhosts ${domains}\ntimeout 30\nuser root\ngroup root";
+    config  => "env.vhosts ${domains}";
+  }
+  File {
+    require => Munin::Plugin['ejabberd_registrations'],
+    owner   => root,
+    group   => 0,
+    mode    => '0700',
   }
   file{
     '/etc/cron.daily/ejabberd_registrations':
-      source  => 'puppet:///modules/ejabberd/munin/ejabberd_registrations.cron',
-      require => Munin::Plugin['ejabberd_registrations'],
-      owner   => root,
-      group   => 0,
-      mode    => '0700';
+      source  => 'puppet:///modules/ejabberd/munin/ejabberd_registrations.cron';
+    '/etc/cron.d/ejabberd_munin':
+      source  => 'puppet:///modules/ejabberd/munin/ejabberd_munin.cron';
   }
 }
