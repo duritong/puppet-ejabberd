53e9086ab111919e009ccdbceba191ec7a7823a4
mh <mh@immerda.ch>
2021-04-26 20:32:29 +0200

    linting

--- manifests/base.pp
+++ manifests/base.pp
@@ -1,35 +1,34 @@
 # manages the basic stuff for the service
 class ejabberd::base {
-
-  package{'ejabberd':
+  package { 'ejabberd':
     ensure => installed,
-  } -> file_line{'set_erlang_node_name':
-    line   => "ERLANG_NODE=ejabberd@${facts['fqdn']}",
+  } -> file_line { 'set_erlang_node_name':
+    line   => "ERLANG_NODE=ejabberd@${facts['networking']['fqdn']}",
     path   => '/opt/ejabberd/conf/ejabberdctl.cfg',
     match  => '^(#)?ERLANG_NODE=',
     notify => Service['ejabberd'],
-  } -> file{'/opt/ejabberd/conf/ejabberd.yml':
+  } -> file { '/opt/ejabberd/conf/ejabberd.yml':
     owner  => 'root',
     group  => 'ejabberd',
     mode   => '0640',
     notify => Service['ejabberd'],
-  } -> exec{'copy-ejabberd-service-unit':
+  } -> exec { 'copy-ejabberd-service-unit':
     command => "cp /opt/ejabberd-$(rpm -q --queryformat='%{VERSION}' ejabberd)/bin/ejabberd.service /etc/systemd/system/ejabberd.service",
     unless  => "diff -Naur /opt/ejabberd-$(rpm -q --queryformat='%{VERSION}' ejabberd)/bin/ejabberd.service /etc/systemd/system/ejabberd.service",
-  } ~> service{'ejabberd':
-    ensure  => running,
-    enable  => true,
+  } ~> service { 'ejabberd':
+    ensure => running,
+    enable => true,
   }
 
   if $ejabberd::config_content {
-    File['/opt/ejabberd/conf/ejabberd.yml']{
+    File['/opt/ejabberd/conf/ejabberd.yml'] {
       content => $ejabberd::config_content,
     }
   } else {
-    File['/opt/ejabberd/conf/ejabberd.yml']{
-      source => [ "puppet:///modules/site_ejabberd/${facts['fqdn']}/ejabberd.yml",
-                  'puppet:///modules/site_ejabberd/ejabberd.yml',
-                  'puppet:///modules/ejabberd/ejabberd.yml', ]
+    File['/opt/ejabberd/conf/ejabberd.yml'] {
+      source => ["puppet:///modules/site_ejabberd/${facts['networking']['fqdn']}/ejabberd.yml",
+        'puppet:///modules/site_ejabberd/ejabberd.yml',
+      'puppet:///modules/ejabberd/ejabberd.yml',]
     }
   }
 }
